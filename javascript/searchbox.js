96dd83922653c57c6ce504b034fcf26ba38f917e
Dan Costa <dancosta.id@gmail.com>
2020-09-29 09:52:56 +1300

    YASSSSS
    I believe I've done it. I guess I made it work.

--- javascript/searchbox.js
+++ javascript/searchbox.js
@@ -1,22 +1,27 @@
 /* ============================================= */
 /*              Search Button                    */
 /* ============================================= */
-
-    const searchBox = document.getElementById('search');
-    let gallery = document.getElementsByClassName('item-gallery');
-    let strainer = search.value.toUpperCase();
-    searchBox.addEventListener("keyup", e => { 
-        const searchString = e.target.value; 
-      });
-
     
+// Getting elements from the input
+const searchBox = document.getElementById('search');
+let gallery = document.getElementsByClassName('item-gallery');
+    
+    
+// User input value
+searchBox.addEventListener("keyup", e => { 
+    const searchString = e.target.value.toUpperCase(); 
+    
+    // for any input check the data caption (upperCase sensitive)
     for (let i = 0; i < gallery.length; i++) {
-        const upperCaseInput = gallery[i].getAttribute('data-caption').toUpperCase()
+        const userInput = gallery[i].getAttribute('data-caption').toUpperCase()
 
-        if (upperCaseInput.includes(strainer)) {
+        // if user includes letters or name block the image on screen
+        if (userInput.includes(searchString)) {
             gallery[i].style.display = 'block';
-    
+            
+            // if nothing matches display no image
         } else {
             gallery[i].style.display = 'none';
         }
     }
+});
